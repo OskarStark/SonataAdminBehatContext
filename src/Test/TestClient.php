c32445d83ddd222aee24da6de047ef331cbd4bfe
Oskar Stark <oskarstark@googlemail.com>
2018-12-21 06:55:37 +0100

    use header comment

--- src/Test/TestClient.php
+++ src/Test/TestClient.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SonataAdminBehatContext package.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace OStark\Test;
 
 use Symfony\Component\BrowserKit\Client;
